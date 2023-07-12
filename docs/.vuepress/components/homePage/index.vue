ec7c2e4661d6a8f5a3db99e18b633fa4c69d4740
XJ <1163464003@qq.com>
2023-07-12 16:49:23 +0800

    feat

--- docs/.vuepress/components/homePage/index.vue
+++ docs/.vuepress/components/homePage/index.vue
@@ -3,6 +3,11 @@
 		<h1>Fake Element Plus</h1>
 		<p class="wrap-desc">这是一个仿造ElementPlus的vuepress文档</p>
 		<img src="../../public/images/theme-index-blue.png" alt="">
+		<div class="wrap-cardBox">
+			<itemCard></itemCard>
+			<itemCard></itemCard>
+			<itemCard></itemCard>
+		</div>
 	</div>
 </template>
 <script setup>
@@ -16,11 +21,19 @@ defineOptions({
 	padding-top: 55px;
 	flex-direction: column;
 	align-items: center;
-	h1{
+
+	h1 {
 		font-weight: 600;
 	}
+
 	&-desc {
 		margin-top: 0px !important;
+	}
+	&-cardBox{
+		width: 100%;
+		margin-top: 50px;
+		display: flex;
+		justify-content: space-around;
 	}
 }
 </style>
---
+++ docs/.vuepress/components/itemCard/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<el-card shadow="never" class="card">
+		<img :src="imgPath" alt="">
+		<slot name="title"></slot>
+		<slot></slot>
+		<div class="card-footer">
+			<a href="">查看详情</a>
+		</div>
+	</el-card>
+</template>
+<script setup>
+defineOptions({
+	name: 'itemCard'
+})
+const props = defineProps({
+	imgPath: String, // 
+})
+</script>
+<style lang='scss'>
+.card {
+	width: 220px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	.el-card__body {
+		width: 100%;
+	}
+
+	&-footer {
+		width: 100%;
+		border-top: 1px solid #000;
+		padding-top: 20px;
+	}
+}
+</style>
